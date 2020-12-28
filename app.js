41ad796702c393e5cc200c8009456e1c9fe92605
rakakhrl <raka.khairil@gmail.com>
2020-12-28 11:38:09 +0700

    Use cors middleware

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 const port = process.env.PORT;
 const path = require("path");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, `uploads`)));
