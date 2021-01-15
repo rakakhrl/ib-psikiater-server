0c622ed3bb228a1c1ebc79c4fc797e7e37d766e1
fauzihaqmuslim <uji.haqmuslim@gmail.com>
2021-01-15 13:58:49 +0700

    add field schema isOnline

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 require("dotenv").config();
+// const Appointments = require("./models/appoinments");
 
 const router = require("./routes");
 const express = require("express");
@@ -18,6 +19,47 @@ app.use(express.static(path.join(__dirname, `uploads`)));
 app.use("/media/:filename", (req, res) => {
   res.sendFile(path.join(__dirname, `/uploads/${req.params.filename}`));
 });
+
+// app.post("/appoinments-tes", async (req, res) => {
+//   const {
+//     psikiater_id,
+//     patient_id,
+//     prescription_id,
+//     appointment_date,
+//     appointment_time,
+//     complaint,
+//     status,
+//     isOnline,
+//     allergy,
+//     diagnose,
+//     diagnose_name,
+//     diagnose_date,
+//   } = req.body;
+//   try {
+//     const appoinmentsData = {
+//       psikiater_id: psikiater_id,
+//       patient_id: patient_id,
+//       prescription_id: prescription_id,
+//       appointment_date: appointment_date,
+//       appointment_time: appointment_time,
+//       complaint: complaint,
+//       status: status,
+//       allergy: allergy,
+//       isOnline: isOnline,
+//       diagnose: diagnose,
+//       diagnose_name: diagnose_name,
+//       diagnose_date: diagnose_date,
+//     };
+//     const appoinments = await Appointments.create(appoinmentsData);
+//     res.status(200).json({
+//       status: "success",
+//       message: "success created data",
+//       data: appoinments,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 app.use(router);
 
