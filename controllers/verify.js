faa4b51a4d41b5ade0dfab5741ff5a592c723a3b
fauzihaqmuslim <uji.haqmuslim@gmail.com>
2021-01-18 16:46:42 +0700

    create endpoint verification email

---
+++ controllers/verify.js
@@ -0,0 +1,27 @@
+const VerifyModel = require("../models/verify");
+const jwt = require("jsonwebtoken");
+
+class VerifyController {
+  static verifyUser = async (req, res, next) => {
+    try {
+      const { token } = req.params;
+      const verifyData = await VerifyModel.findOne({
+        token: token,
+      });
+
+      if (!verifyData) {
+        throw new Error("Verification failed");
+      }
+
+      if (verifyData && token) {
+        res.status(204).send({
+          message: "success verification",
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+module.exports = VerifyController;
