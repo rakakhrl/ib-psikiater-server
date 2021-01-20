761476993c03315ab8d7167cdbd8d696aae8430f
rakakhrl <raka.khairil@gmail.com>
2021-01-20 14:20:44 +0700

    Implement rating agregate pipeline and revert review endpoint position

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,11 @@ Router.use("/psikiater", psikiaterRouter);
 
 Router.use("/verify-user", verifyRouter);
 
-Router.use("/reviews", reviewRouter);
-
 Router.use(authentication);
 
 Router.use("/appointments", appointmentRouter);
+
+Router.use("/reviews", reviewRouter);
 
 Router.use("/patients", patientRouter);
 
