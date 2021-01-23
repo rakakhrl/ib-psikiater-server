232a0b3230eb78fd7dbcd14f4adcab34327a57f4
Naufal Al Fachri <nafa.create@gmail.com>
2021-01-24 02:05:54 +0700

    membuat endpoint untuk check psikiater time schedule di hari tertentu

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,6 +2,26 @@
 const AppointmentModel = require("../models/appointments");
 
 class AppointmentController {
+  static getAppointmentPsikiaterSchedule = async (req, res, next) => {
+    const { psikiater_id, appointment_date } = req.query;
+    try {
+      const appointmentData = await AppointmentModel.find({
+        $and: [
+          { psikiater_id: psikiater_id },
+          { appointment_date: appointment_date },
+        ],
+      });
+
+      res.status(200).json({
+        status: "Success",
+        message: "Success Get Psikiater Time Schedule",
+        data: appointmentData.map((item) => item.appointment_time),
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
   static getOneAppointmentDataByAppointmentId = async (req, res, next) => {
     try {
       const { appointment_id } = req.params;
