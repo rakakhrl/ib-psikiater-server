6495c602e87e15844bf18a8e362fcd28135d05d9
Naufal Al Fachri <nafa.create@gmail.com>
2021-01-24 11:42:18 +0700

    edit paymentCheckout on paymentcontroller

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,35 +1,26 @@
 const PaymentModel = require("../models/payments");
 
 class PaymentController {
-  static paymentCheckout = async (req,res,next)=>{
-    const {
-        patient,
-        payment_type,
-        amount,
-        payment_method,
-        payment_status,
-        slip_url,
-    }= req.body;
+  static paymentCheckout = async (req, res, next) => {
+    const { patient, product_type, product_detail, product_price } = req.body;
 
     try {
-        const paymentData = {
-            patient:patient,
-            payment_type:payment_type,
-            amount:amount,
-            payment_method:payment_method,
-            payment_status:payment_status,
-            slip_url:slip_url, 
-        };
-        const payment = await PaymentModel.create(paymentData);
-        res.status(201).json({
-            status: "Success",
-            message: "Checkout success",
-            data: payment,
-        })
+      const paymentData = {
+        patient: patient,
+        product_type: product_type,
+        product_detail: product_detail,
+        product_price,
+      };
+      const payment = await PaymentModel.create(paymentData);
+      res.status(201).json({
+        status: "Success",
+        message: "Checkout success",
+        data: payment,
+      });
     } catch (error) {
-        next(error);
+      next(error);
     }
-}
+  };
   static getOneById = async (req, res, next) => {
     try {
       const { payment_id } = req.params;
