50525e515feae1a7ca9ba6c7a60e5234d356901c
Dheal <dlufiga@gmail.com>
2021-01-25 15:21:52 +0700

    update Accept/Reject Payment Endpoint

--- routes/admin.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express").Router();
-const AdminController = require("../controllers/admin");
-// const authorization = require("../middlewares/authorization");
-// const { PATIENT } = require("../constants/role");
-
-Router.patch("/status",AdminController.updateStatusByAppointmentID);
-
-module.exports = Router;
--- routes/admins.js
+++ routes/admins.js
@@ -2,5 +2,6 @@ const express = require("express");
 const Router = express.Router();
 const AdminController = require("../controllers/admins");
 
+Router.patch("/status",AdminController.updateStatusByAppointmentID);
 Router.post("/create-admin", AdminController.createAdmin);
 module.exports = Router;
