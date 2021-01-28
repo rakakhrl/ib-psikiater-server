1eb21d369e586d25e7a2154ea243069af4376ea3
Naufal Al Fachri <nafa.create@gmail.com>
2021-01-28 22:46:29 +0700

    menghapus authorization pada route payment pending

--- routes/payments.js
+++ routes/payments.js
@@ -4,7 +4,7 @@ const upload = require("../middlewares/multer");
 const authorization = require("../middlewares/authorization");
 const { PATIENT, ADMIN } = require("../constants/role");
 
-Router.get("/pending", authorization(ADMIN), PaymentController.getAllPending);
+Router.get("/pending", PaymentController.getAllPending);
 Router.get("/:payment_id", PaymentController.getOneById);
 
 Router.use(authorization(PATIENT));
